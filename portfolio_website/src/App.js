14b9de19eefb678f892f28191aad2cdcd4fa8b8d
Samuel Gustin <gustin.daniel1@gmail.com>
2020-11-11 03:55:21 +0200

    Service component
    + singular service component

--- portfolio_website/src/App.js
+++ portfolio_website/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import Header from './components/header/Header'
 import Introduction from './components/introduction/Introduction'
+import Services from "./components/services/Services";
 // import Services from './components/Services'
 // import Footer from './components/Footer'
 
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <Header />
         <Introduction />
+        <Services />
       </div>
   )
 }
