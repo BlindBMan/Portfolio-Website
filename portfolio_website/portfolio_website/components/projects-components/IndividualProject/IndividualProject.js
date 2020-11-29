0f9af6232d26f35b1d9e6fdfe8c7146c3dc6a3f3
Samuel Gustin <gustin.daniel1@gmail.com>
2020-11-29 08:10:01 +0200

    further routing
    made projects clickable with nextjs routing
    todo: make dynamic routing

--- portfolio_website/portfolio_website/components/projects-components/IndividualProject/IndividualProject.js
+++ portfolio_website/portfolio_website/components/projects-components/IndividualProject/IndividualProject.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './IndividualProject.module.css'
+import Link from "next/link";
 
 export default function IndividualProject(props) {
     function handleClick() {
@@ -7,29 +8,33 @@ export default function IndividualProject(props) {
     }
 
     return (
-        <div className={`${styles.individualProject}`}>
-            <img src={props.imgSrc}
-                 alt={props.title}
-                 onClick={handleClick}
-                 className={styles.projImg}
-            />
+        <Link href={'/specificProject'}>
+            <a>
+                <div className={`${styles.individualProject}`}>
+                    <img src={props.imgSrc}
+                         alt={props.title}
+                         onClick={handleClick}
+                         className={styles.projImg}
+                    />
 
-            <div className={`${styles.overlay} d-flex flex-column`}>
-                <div
-                    className={`align-self-center`}
-                    style={{
-                            position: "absolute",
-                            top: "50%",
-                            left: "50%",
-                            fontSize: "20px",
-                            color: "black",
-                            fontWeight: "bold",
-                            textAlign: "center",
-                            transform: "translate(-50%, -50%)",
-                            opacity: "1"
-                    }}
-                >{props.title}</div>
-            </div>
-        </div>
+                    <div className={`${styles.overlay} d-flex flex-column`}>
+                        <div
+                            className={`align-self-center`}
+                            style={{
+                                position: "absolute",
+                                top: "50%",
+                                left: "50%",
+                                fontSize: "20px",
+                                color: "black",
+                                fontWeight: "bold",
+                                textAlign: "center",
+                                transform: "translate(-50%, -50%)",
+                                opacity: "1"
+                            }}
+                        >{props.title}</div>
+                    </div>
+                </div>
+            </a>
+        </Link>
     )
 }
