6e6528686b37a426b8aba50975caca06d3c1f83b
Samuel Gustin <gustin.daniel1@gmail.com>
2020-11-29 06:41:02 +0200

    contact page migrated

--- portfolio_website/portfolio_website/components/email-form/EmailForm.js
+++ portfolio_website/portfolio_website/components/email-form/EmailForm.js
@@ -1,48 +1,53 @@
 import React from 'react'
-import './EmailForm.module.css'
+import styles from './EmailForm.module.css'
 import CustomBtn from "../CustomBtn/CustomBtn";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/cjs/Row";
 
 export default function EmailForm() {
     return (
-        <div id="email-form" className=".container">
-            <div className="container d-flex flex-column">
-                <div className="d-flex flex-column row">
-                    <h1 className="title">
+        <Container fluid className={`${styles.emailForm} h-100`}>
+            <div className="container d-flex flex-column h-100">
+                <Row className="d-flex flex-column">
+                    <h1 className={styles.title}>
                         Contact
                     </h1>
-                    <p className="description align-self-center">
+                    <p className={`${styles.description} align-self-center`}>
                         This are some, but not limited to, of the services I can provide for my clients.
                         Topped by fast and bug free delivery.
                     </p>
-                </div>
+                </Row>
 
-                <div className="row d-flex flex-column">
-                    <div className="row justify-content-center input-container">
+                <Row className="d-flex flex-column">
+                    <Row className={`justify-content-center ${styles.inputContainer}`}>
                         <input
-                            className="col-sm-4"
+                            className={`col-sm-4 ${styles.inpt}`}
                             placeholder="Full name"
                         />
                         <input
-                            className="col-sm-4"
+                            className={`col-sm-4 ${styles.inpt} ${styles.inputMargin}`}
                             placeholder="Email"
                         />
-                    </div>
+                    </Row>
 
-                    <div className="row justify-content-center">
+                    <Row className="justify-content-center">
                         <textarea
-                            className="col-sm-9"
+                            className={`col-sm-9 ${styles.textareaStyle}`}
                             placeholder="Your message here..."
                         />
-                    </div>
+                    </Row>
 
-                </div>
+                </Row>
 
-                <div className="d-flex flex-column row my-btn">
+                <Row
+                    className={`d-flex flex-column`}
+                    style={{marginBottom: "4.57vh"}}
+                >
                     <div className="align-self-center">
                         <CustomBtn content="Email me!"/>
                     </div>
-                </div>
+                </Row>
             </div>
-        </div>
+        </Container>
     )
 }
